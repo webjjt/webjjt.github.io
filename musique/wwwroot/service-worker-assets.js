d9574e1ea4286cbac98fdcff12d5edec02f005e5
webjjt <147999395+webjjt@users.noreply.github.com>
2023-10-17 16:03:16 +0200

    essai

--- musique/wwwroot/service-worker-assets.js
+++ musique/wwwroot/service-worker-assets.js
@@ -261,7 +261,7 @@
       "url": "icon-512.png"
     },
     {
-      "hash": "sha256-rNZ2SwWbHBq0+4uG2Eko0\/9Z5dut0GjEPNl484CRm4A=",
+      "hash": "sha256-9fbVRRqc1cpO3br5DzamRHWvuFTzmC0S6DqAOxmJMsw=",
       "url": "index.html"
     },
     {
@@ -273,5 +273,5 @@
       "url": "sample-data\/weather.json"
     }
   ],
-  "version": "GuSZWMmx"
+  "version": "MiP8zQIx"
 };
