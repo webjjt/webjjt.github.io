4708cec5e8d0f58c5e467c06a67fb93367ef7f4a
webjjt <147999395+webjjt@users.noreply.github.com>
2024-10-29 11:31:25 +0100

    202410291131

--- jwdp/wwwroot/service-worker-assets.js
+++ jwdp/wwwroot/service-worker-assets.js
@@ -29,7 +29,7 @@
       "url": "_framework\/icudt_no_CJK.dat"
     },
     {
-      "hash": "sha256-Ey4q8CXNsYtCSgMCiyNlsY2+raGhLlw08YiplWzK8zk=",
+      "hash": "sha256-rJLhKTEvM6cNqDhdASn1NYgFDWmNo9qTIu2HNJlm6+w=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -37,7 +37,7 @@
       "url": "_framework\/Blazored.LocalStorage.dll"
     },
     {
-      "hash": "sha256-VT4iDkzRbGCyasI8Bply\/7FB7MOb+45Xl7X9Ewau5pA=",
+      "hash": "sha256-mDZ+Rz9IYtHCb2p0apo6Xyt6nH+F6TYY9LE6QHTF76o=",
       "url": "_framework\/jwdp.Client.dll"
     },
     {
@@ -477,5 +477,5 @@
       "url": "manifest.json"
     }
   ],
-  "version": "Cgj45GgG"
+  "version": "19pbipNq"
 };
--- jwdp/wwwroot/service-worker.js
+++ jwdp/wwwroot/service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: Cgj45GgG */
+/* Manifest version: 19pbipNq */
