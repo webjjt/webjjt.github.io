8113d73b06f2a4c6c52a7858ff10bc9eb59c928b
webjjt <147999395+webjjt@users.noreply.github.com>
2026-02-04 12:25:48 +0100

    20260204

--- comptes/wwwroot/service-worker-assets.js
+++ comptes/wwwroot/service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "oB0XO8wq",
+  "version": "OWpD3QdU",
   "assets": [
     {
       "hash": "sha256-9NXJ841I9AsGwCwIqsULlS8C4+fE/N2bYWOctiv/vs8=",
@@ -358,7 +358,7 @@ self.assetsManifest = {
       "url": "icon-192.png"
     },
     {
-      "hash": "sha256-1Q4RzUZDrQQFx/eNoCPyHSpDKe1MZIe3Qi7M9I7d/N0=",
+      "hash": "sha256-kR/pc3QInCfM2VRalHBP0zdaAk7lRmCQSrIYeNKUTEU=",
       "url": "icon-512.png"
     },
     {
