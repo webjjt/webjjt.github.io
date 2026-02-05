b8e1e09efa2f825f04ee8e7bb3ccc398f9187b16
webjjt <147999395+webjjt@users.noreply.github.com>
2026-02-05 19:54:56 +0100

    2026020504

--- comptes/wwwroot/service-worker.js
+++ comptes/wwwroot/service-worker.js
@@ -20,6 +20,9 @@ const manifestUrlList = self.assetsManifest.assets.map(asset => new URL(asset.ur
 async function onInstall(event) {
     console.info('Service worker: Install');
 
+    //ajout pour suivre les mises � jour
+    self.skipWaiting();
+
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
