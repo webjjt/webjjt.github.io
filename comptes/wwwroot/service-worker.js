5508e6ca6b728b581fc0515fa4f0a5ea440463c9
webjjt <147999395+webjjt@users.noreply.github.com>
2026-02-09 18:44:39 +0100

    202602091844

--- comptes/wwwroot/service-worker.js
+++ comptes/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: HmAnHudP */
+/* Manifest version: +05AR7JA */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
